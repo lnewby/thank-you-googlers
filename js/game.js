3fc2ccb52829423b502cbc553171b713f8a5041f
Lance Newby <lnewby@gmail.com>
2021-04-12 14:36:53 -0400

    fix mobile browser message

--- js/game.js
+++ js/game.js
@@ -718,10 +718,11 @@ function mobileCheck() {
 
 function messageForMobile() {
 
-    // if (mobileCheck()) {
-    //     document.getElementById('main').style.display = 'none';
-    //     document.getElementById('mobile-device-msg').classList.remove('hidden');
-    // }
+    if (mobileCheck()) {
+        document.getElementById('main').style.display = 'none';
+        document.getElementById('mobile-device-msg').classList.remove('hidden')
+        document.getElementById('mobile-device-msg').style.display = 'flex';
+    }
 }
 
 //
